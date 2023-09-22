8b576d6b9310cf8793636931c625cda2993c5d78
Dmitri Jrapach <dmitrijrapach@gmail.com>
2023-09-22 15:22:39 -0300

    preentrega uno

--- js/main.js
+++ js/main.js
@@ -3,39 +3,73 @@ let raza = prompt("Seleccione raza: (Puede elegir entre: elfo, humano, enano, gn
 switch(raza){
 
     case "enano":
-        console.log("Las clases predilectas para enano son: Guerrero, Clerigo ");
+        console.log("Raza: Enano, las clases predilectas para enano son: Guerrero, Clerigo ");
         break;
 
     case "elfo":
-        console.log("Las clases predilectas para elfo son: Explorador, Druida, Guerrero");
+        console.log("Raza: Elfo, las clases predilectas para elfo son: Explorador, Druida, Guerrero");
         break;
 
     case "mediano":
-        console.log("Las clases predilectas para mediano son: Picaro, Hechicero, Bardo");
+        console.log("Raza: Mediano, las clases predilectas para mediano son: Picaro, Hechicero, Bardo");
         break;
 
     case "humano":
-        console.log("Los humanos no tienen clase predilecta");
+        console.log("Raza: Humano, los humanos no tienen clase predilecta");
         break;
 
     case "gnomo":
-        console.log("Las clases preidlectas para gnomo son: Picaro, Druida, Mago");
+        console.log("Raza: Gnomo, las clases preidlectas para gnomo son: Picaro, Druida, Mago");
         break;
 
     case "semielfo":
-        console.log("Las clases predilectas para semielfo son : Bardo, Brujo, Paladin, Hechicero");
+        console.log("Raza: Semielfo, las clases predilectas para semielfo son : Bardo, Brujo, Paladin, Hechicero");
         break;
     case "semiorco":
-        console.log("Las clases predilectas para semiorco son : Barbaro, Guerrero, Explorador");
+        console.log("Raza: Semiorco, las clases predilectas para semiorco son : Barbaro, Guerrero, Explorador");
         break;
 
     default:
-        prompt("Ingrese una raza")
+        prompt("Ingrese una raza");
 }
 
-// let clase = prompt("Seleccione una clase: (Eliga entre: Barbaro, Bardo, Brujo, Clerigo, Druida, Explorador, Guerrero, Hechicero, Mago, Monje, Paldin, Picaro )")
+let clase = prompt("Seleccione una clase: (Eliga entre: barbaro, bardo, brujo, clerigo, druida, explorador, guerrero, hechicero, mago, monje, paldin, picaro )")
 
-// switch(raza){
+if (clase !== " " && clase == "barbaro" || "bardo" || "brujo" || "clerigo" || "druida" || "explorador" || "guerrero" || "hechicero" || "mago" || "monje" || "paladin" || "picaro") {
+    console.log("clase: " + clase);
+}
+else{
+    alert("seleccione una clase");
+}
 
-//     case "Barbaro||barbaro"
-// }
+let total;
+
+do{
+    let fuerza = Number(prompt("ingrese su fuerza"));
+    console.log("Fuerza: " + fuerza);
+
+    let destreza = Number(prompt("ingrese su destreza"));
+    console.log("Destreza: " + destreza);
+
+    let constitucion = Number(prompt("ingrese su constitucion"));
+    console.log("Constitucion: " + constitucion);
+
+    let sabiduria = Number(prompt("ingrese su sabiduria"));
+    console.log("Sabiduria: " + sabiduria);
+
+    let inteligencia = Number(prompt("ingrese su inteligencia"));
+    console.log("Inteligencia: " + inteligencia);
+
+    let carisma = Number(prompt("ingrese su carisma"));
+    console.log("Carisma: " + carisma);
+
+    total = fuerza + destreza + constitucion + sabiduria + inteligencia + carisma ;
+
+    if (total > 78) {
+    console.log("El total de puntos es mayor o igual a 78. Vuelva a repartir los puntos.");
+    }
+    else {
+    console.log("Los datos son correctos.");
+    }
+}
+while (total > 78);
